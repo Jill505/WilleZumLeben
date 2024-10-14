130f169f5571d7e71d9d7fae1b22a32917039c35
Jill505 <orangec.org@gmail.com>
2024-10-14 14:59:49 +0800

    version update

--- Assets/Resources/experiment/exp_example.cs
+++ Assets/Resources/experiment/exp_example.cs
@@ -7,7 +7,8 @@ public class exp_example : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("experiment debug log information");
+        Debug.Log("¦­¦w©@°Ø");
     }
 
     // Update is called once per frame
