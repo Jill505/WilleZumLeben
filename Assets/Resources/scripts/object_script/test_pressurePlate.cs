5a68c401cf8e2698988f63f8e2e78f033acfe40c
Jill505 <orangec.org@gmail.com>
2024-11-25 13:56:35 +0800

    update a little bit

--- Assets/Resources/scripts/object_script/test_pressurePlate.cs
+++ Assets/Resources/scripts/object_script/test_pressurePlate.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class test_pressurePlate : MonoBehaviour
 {
     public GameObject dialogContext;
-
+    public Dialogue_Core diaCore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        diaCore = GameObject.Find("ConverSysMaster").GetComponent<Dialogue_Core>();
     }
 
     // Update is called once per frame
@@ -19,14 +19,20 @@ public class test_pressurePlate : MonoBehaviour
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        if (diaCore == null)
+        {
+            //try find diaCore
+            diaCore = GameObject.Find("ConverSysMaster").GetComponent<Dialogue_Core>();
+        }
+
 
         if (collision.gameObject.tag == "John")
         {
             Debug.Log("Ä²µo¾÷Ãö");
             if (dialogContext != null)
             {
-                GameObject.Find("ConverSysMaster").GetComponent<Dialogue_Core>().hanas = dialogContext.GetComponent<DialogContextObject>().dialog;
-                GameObject.Find("ConverSysMaster").GetComponent<Dialogue_Core>().startDialog();
+                diaCore.hanas = dialogContext.GetComponent<DialogContextObject>().dialog;
+                diaCore.startDialog();
             }
         }
     }
