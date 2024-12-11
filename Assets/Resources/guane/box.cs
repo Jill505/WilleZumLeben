c76dfc2f7c71d3dcaf6c3057d6dfd0141667fecb
sv0502 <wuososca@gmail.com>
2024-12-11 16:36:06 +0800

    我剛剛傳錯了

--- Assets/Resources/guane/box.cs
+++ Assets/Resources/guane/box.cs
@@ -2,28 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enimytry : MonoBehaviour
+public class box : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    public float flyForce = 10f;// 控制箱子飛出的力量
+    public Rigidbody2D rb;
 
-    public void Die()
-    {
-        Debug.Log("敵人葛屁");
-        Destroy(gameObject);//刪除敵人物件
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //確認是否是那箱子
-        if (collision.gameObject.CompareTag("box")) ;
-        {
-            Die();
-        }
-    }
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        Fly(5f);
     }
+    public void Fly(float direction)
+    {
+        Vector2 force = new Vector2(direction, 0).normalized * flyForce; // 根據方向計算箱子飛出的力量
+        rb.AddForce(force, ForceMode2D.Impulse);// 添加衝擊力
+    }
+
 
     // Update is called once per frame
     void Update()
